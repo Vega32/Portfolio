a0d6c16e6924b0fc6a6c3bcdafaf841f53029a97
Vega64 <omar.hammodan@gmail.com>
2025-04-06 19:14:27 -0400

    update

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
             <img className='sunset' src={SynthSunset}/>
         </div>
         <div className='about_p'>
-            <h1>I'm Omar, the Cyberspace Journeyman</h1>
+            <h1>I'm Omar</h1>
             <p>I’m a software engineer student with multiple years of software development experience under my belt. I’ve always been very passionate about this field. I’ve work on countless projects which has led me to experiment with all sorts of languages and technologies. This includes a wide range from low level development with C to high level with the likes of Python. I’m pretty resourceful when it comes to problem solving and can quickly learn the skills needed for a new endeavor. Here are just a couple of projects I’ve worked on. </p>
         </div>
     </div>
